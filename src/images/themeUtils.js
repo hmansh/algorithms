57d1936afc496e7d2864eac0d1268e641c6efcc0
hmansh <himanshucasillas@gmail.com>
2022-01-26 00:51:29 +0530

    New Repo for Learning

--- src/images/themeUtils.js
+++
@@ -1,7 +0,0 @@
-export const pxToRem = (px) => `${px / 18.018}rem`;
-
-export const spacing = (value) => `${pxToRem(value)}`;
-
-export const pxToVh = (px, portHeight = 940) => `${(px * 100) / portHeight}vh`;
-
-export const pxToVw = (px, portWidth = 1920) => `${(px * 100) / portWidth}vw`;
